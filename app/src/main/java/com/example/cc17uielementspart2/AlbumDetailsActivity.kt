951c13e1c2c3ec8b81ed140f2403a1aebc1dba83
Princess <pcjbautista@gmail.com>
2020-11-17 09:05:38 -0800

    added album details activity

--- app/src/main/java/com/example/cc17uielementspart2/AlbumDetailsActivity.kt
+++ app/src/main/java/com/example/cc17uielementspart2/AlbumDetailsActivity.kt
@@ -2,10 +2,35 @@ package com.example.cc17uielementspart2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
 
 class AlbumDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_album_details)
+
+        val uri = intent.getStringExtra("imageUri")
+        val title = intent.getStringExtra("albumTitle")
+        val artist = intent.getStringExtra("albumArtist")
+        val albumSongs = intent.getStringArrayListExtra("songs")
+
+        var songsArray = albumSongs!!.toTypedArray()
+        val albumDetailsList = findViewById<ListView>(R.id.albumDetailsList)
+        val AlbumCover = findViewById<ImageView>(R.id.albumCover)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songsArray)
+        albumDetailsList.adapter = adapter
+        var imageResource = getResources().getIdentifier(uri, null, getPackageName())
+        var res = getResources().getDrawable(imageResource)
+        AlbumCover.setImageDrawable(res)
+
+        val albumTitle = findViewById<TextView>(R.id.albumTitle)
+        val albumArtist = findViewById<TextView>(R.id.albumArtist)
+        albumTitle.setText(title)
+        albumArtist.setText(artist)
+
+
     }
 }
--- app/src/main/java/com/example/cc17uielementspart2/AlbumsActivity.kt
+++ app/src/main/java/com/example/cc17uielementspart2/AlbumsActivity.kt
@@ -3,6 +3,7 @@ package com.example.cc17uielementspart2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.AdapterView
 import android.widget.GridView
 
@@ -19,21 +20,32 @@ class AlbumsActivity : AppCompatActivity() {
             val intent = Intent(this, AlbumDetailsActivity::class.java)
             var albumSongs = arrayListOf<String>()
             var uri: String = ""
+            var albumTitle: String = ""
+            var albumArtist: String = ""
             if (position == 0) {
                 uri = "@drawable/kidkrow"
+                albumTitle = getString(R.string.album1)
+                albumArtist = getString(R.string.artist1)
+                Log.i("album", "albumname: $albumTitle")
                 albumSongs.clear()
                 albumSongs.addAll(resources.getStringArray(R.array.kidKrow))
             } else if (position == 1) {
                 uri = "@drawable/nswy"
+                albumTitle = getString(R.string.album2)
+                albumArtist = getString(R.string.artist2)
                 albumSongs.clear()
                 albumSongs.addAll(resources.getStringArray(R.array.noSongWithoutYou))
             } else {
                 uri = "@drawable/odaat"
+                albumTitle = getString(R.string.album3)
+                albumArtist = getString(R.string.artist3)
                 albumSongs.clear()
                 albumSongs.addAll(resources.getStringArray(R.array.oneDayAtATime))
             }
             intent.putStringArrayListExtra("songs", albumSongs)
             intent.putExtra("imageUri", uri)
+            intent.putExtra("albumTitle", albumTitle)
+            intent.putExtra("albumArtist", albumArtist)
             intent.putExtra("position", position)
             startActivity(intent)
         }
