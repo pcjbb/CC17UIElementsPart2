f37ed07ad1724e040a282138cab7c43e4322b95f
Princess <pcjbautista@gmail.com>
2020-11-17 05:11:24 -0800

    menu bar

--- app/src/main/java/com/example/cc17uielementspart2/MainActivity.kt
+++ app/src/main/java/com/example/cc17uielementspart2/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.cc17uielementspart2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
 }
