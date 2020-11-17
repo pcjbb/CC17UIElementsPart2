8676a7e792d7ab799dfd75336e32a3480bb2b4b8
Princess <pcjbautista@gmail.com>
2020-11-17 07:29:24 -0800

    added queued songs activity

--- app/src/main/java/com/example/cc17uielementspart2/QueuedSongsActivity.kt
+++ app/src/main/java/com/example/cc17uielementspart2/QueuedSongsActivity.kt
@@ -2,10 +2,19 @@ package com.example.cc17uielementspart2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class QueuedSongsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_queued_songs)
+        val songsQueue = intent.getStringArrayListExtra("songs")
+        val songsQueueArray = songsQueue!!.toTypedArray()
+        val adapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songsQueueArray)
+        val queuedSongsListView = findViewById<ListView>(R.id.songsQueued)
+        queuedSongsListView.adapter = adapter
     }
 }
+
