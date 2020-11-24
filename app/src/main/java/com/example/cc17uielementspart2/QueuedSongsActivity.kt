bec602ac9a1d181c7c1d23677eb2e492861f64a7
Princess <pcjbautista@gmail.com>
2020-11-23 22:47:46 -0800

    notif if queue is empty

--- app/src/main/java/com/example/cc17uielementspart2/QueuedSongsActivity.kt
+++ app/src/main/java/com/example/cc17uielementspart2/QueuedSongsActivity.kt
@@ -3,6 +3,9 @@ package com.example.cc17uielementspart2
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
@@ -17,6 +20,11 @@ import android.widget.Toast
 class QueuedSongsActivity : AppCompatActivity() {
 
     lateinit var adapter: ArrayAdapter<String>
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    private val channelId = "i.apps.notifications"
+    private val description = "Test notification"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,11 +48,42 @@ class QueuedSongsActivity : AppCompatActivity() {
             R.id.remove_song -> {
                 //toast
                 val menuInfo = item.menuInfo as AdapterView.AdapterContextMenuInfo
-                val toast: Toast = Toast.makeText(this, "$songsOnQueue has been removed from QUEUE", Toast.LENGTH_SHORT)
+                val toast: Toast = Toast.makeText(this, "$songsOnQueue has been removed from QUEUE", Toast.LENGTH_LONG)
+
                 //removes song from queue
                 songsOnQueue.removeAt(menuInfo.position)
                 adapter.notifyDataSetChanged()
                 toast.show()
+
+                //get notif service as notif manager
+                notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                //if the queue is empty
+                if (adapter.isEmpty) {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        notificationChannel = NotificationChannel(
+                                channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                        notificationChannel.enableLights(true)
+                        notificationChannel.lightColor = Color.CYAN
+                        notificationChannel.enableVibration(true)
+                        notificationManager.createNotificationChannel(notificationChannel)
+
+                        builder = Notification.Builder(this, channelId)
+                                .setContentTitle("Queue Emptied")
+                                .setContentText("There are no songs on queue.")
+                                .setSmallIcon(R.drawable.ic_launcher_background)
+
+                    } else {
+                        builder = Notification.Builder(this)
+                                .setContentTitle("Queue Emptied")
+                                .setContentText("There are no songs on queue.")
+                                .setSmallIcon(R.drawable.ic_launcher_background)
+
+                    }
+                    notificationManager.notify(1234, builder.build())
+                }
+
                 true
             }
             else -> super.onContextItemSelected(item)
